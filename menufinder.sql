bef3e523e9c61fc204f0c96e664b3d4616945b70
Marc Sánchez <sanchezfauste@gmail.com>
2016-12-11 15:13:39 +0100

    Adding reviews and ratings

--- menufinder.sql
+++ menufinder.sql
@@ -2,6 +2,10 @@
 -- PostgreSQL database dump
 --
 
+-- Dumped from database version 9.4.9
+-- Dumped by pg_dump version 9.4.9
+-- Started on 2016-12-11 15:11:29 CET
+
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
@@ -10,6 +14,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 
 --
+-- TOC entry 173 (class 1259 OID 17302)
 -- Name: account; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -21,6 +26,7 @@ CREATE TABLE account (
 
 
 --
+-- TOC entry 174 (class 1259 OID 17305)
 -- Name: accounttype; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -30,6 +36,7 @@ CREATE TABLE accounttype (
 
 
 --
+-- TOC entry 175 (class 1259 OID 17308)
 -- Name: item; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -44,6 +51,7 @@ CREATE TABLE item (
 
 
 --
+-- TOC entry 176 (class 1259 OID 17311)
 -- Name: item_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -56,6 +64,8 @@ CREATE SEQUENCE item_id_seq
 
 
 --
+-- TOC entry 2103 (class 0 OID 0)
+-- Dependencies: 176
 -- Name: item_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -63,6 +73,7 @@ ALTER SEQUENCE item_id_seq OWNED BY item.id;
 
 
 --
+-- TOC entry 177 (class 1259 OID 17313)
 -- Name: itemcategory_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -75,6 +86,7 @@ CREATE SEQUENCE itemcategory_id_seq
 
 
 --
+-- TOC entry 178 (class 1259 OID 17315)
 -- Name: itemcategory; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -86,6 +98,42 @@ CREATE TABLE itemcategory (
 
 
 --
+-- TOC entry 188 (class 1259 OID 17504)
+-- Name: itemrating; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE itemrating (
+    id integer NOT NULL,
+    score double precision,
+    account character varying(50),
+    item integer
+);
+
+
+--
+-- TOC entry 187 (class 1259 OID 17502)
+-- Name: itemrating_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE itemrating_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 2104 (class 0 OID 0)
+-- Dependencies: 187
+-- Name: itemrating_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE itemrating_id_seq OWNED BY itemrating.id;
+
+
+--
+-- TOC entry 179 (class 1259 OID 17319)
 -- Name: menu; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -101,6 +149,7 @@ CREATE TABLE menu (
 
 
 --
+-- TOC entry 180 (class 1259 OID 17322)
 -- Name: menu_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -113,6 +162,8 @@ CREATE SEQUENCE menu_id_seq
 
 
 --
+-- TOC entry 2105 (class 0 OID 0)
+-- Dependencies: 180
 -- Name: menu_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -120,6 +171,7 @@ ALTER SEQUENCE menu_id_seq OWNED BY menu.id;
 
 
 --
+-- TOC entry 181 (class 1259 OID 17324)
 -- Name: menuitem; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -131,6 +183,7 @@ CREATE TABLE menuitem (
 
 
 --
+-- TOC entry 182 (class 1259 OID 17327)
 -- Name: restaurant; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -150,6 +203,7 @@ CREATE TABLE restaurant (
 
 
 --
+-- TOC entry 183 (class 1259 OID 17333)
 -- Name: restaurant_id_seq; Type: SEQUENCE; Schema: public; Owner: -
 --
 
@@ -162,6 +216,8 @@ CREATE SEQUENCE restaurant_id_seq
 
 
 --
+-- TOC entry 2106 (class 0 OID 0)
+-- Dependencies: 183
 -- Name: restaurant_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
 --
 
@@ -169,6 +225,53 @@ ALTER SEQUENCE restaurant_id_seq OWNED BY restaurant.id;
 
 
 --
+-- TOC entry 186 (class 1259 OID 17482)
+-- Name: review; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE review (
+    id integer NOT NULL,
+    review text,
+    parent_type character varying(25),
+    parent_id integer,
+    account character varying(50)
+);
+
+
+--
+-- TOC entry 185 (class 1259 OID 17480)
+-- Name: review_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE review_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- TOC entry 2107 (class 0 OID 0)
+-- Dependencies: 185
+-- Name: review_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE review_id_seq OWNED BY review.id;
+
+
+--
+-- TOC entry 184 (class 1259 OID 17475)
+-- Name: reviewtype; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE reviewtype (
+    id character varying(25) NOT NULL
+);
+
+
+--
+-- TOC entry 1933 (class 2604 OID 17335)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -176,6 +279,15 @@ ALTER TABLE ONLY item ALTER COLUMN id SET DEFAULT nextval('item_id_seq'::regclas
 
 
 --
+-- TOC entry 1938 (class 2604 OID 17507)
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY itemrating ALTER COLUMN id SET DEFAULT nextval('itemrating_id_seq'::regclass);
+
+
+--
+-- TOC entry 1935 (class 2604 OID 17336)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -183,6 +295,7 @@ ALTER TABLE ONLY menu ALTER COLUMN id SET DEFAULT nextval('menu_id_seq'::regclas
 
 
 --
+-- TOC entry 1936 (class 2604 OID 17337)
 -- Name: id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -190,12 +303,24 @@ ALTER TABLE ONLY restaurant ALTER COLUMN id SET DEFAULT nextval('restaurant_id_s
 
 
 --
+-- TOC entry 1937 (class 2604 OID 17485)
+-- Name: id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY review ALTER COLUMN id SET DEFAULT nextval('review_id_seq'::regclass);
+
+
+--
+-- TOC entry 2080 (class 0 OID 17302)
+-- Dependencies: 173
 -- Data for Name: account; Type: TABLE DATA; Schema: public; Owner: -
 --
 
 
 
 --
+-- TOC entry 2081 (class 0 OID 17305)
+-- Dependencies: 174
 -- Data for Name: accounttype; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -204,12 +329,16 @@ INSERT INTO accounttype VALUES ('restaurant');
 
 
 --
+-- TOC entry 2082 (class 0 OID 17308)
+-- Dependencies: 175
 -- Data for Name: item; Type: TABLE DATA; Schema: public; Owner: -
 --
 
 
 
 --
+-- TOC entry 2108 (class 0 OID 0)
+-- Dependencies: 176
 -- Name: item_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
@@ -217,6 +346,8 @@ SELECT pg_catalog.setval('item_id_seq', 1, false);
 
 
 --
+-- TOC entry 2085 (class 0 OID 17315)
+-- Dependencies: 178
 -- Data for Name: itemcategory; Type: TABLE DATA; Schema: public; Owner: -
 --
 
@@ -238,6 +369,8 @@ INSERT INTO itemcategory VALUES (15, 'Beer', NULL);
 
 
 --
+-- TOC entry 2109 (class 0 OID 0)
+-- Dependencies: 177
 -- Name: itemcategory_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
@@ -245,12 +378,33 @@ SELECT pg_catalog.setval('itemcategory_id_seq', 15, true);
 
 
 --
+-- TOC entry 2095 (class 0 OID 17504)
+-- Dependencies: 188
+-- Data for Name: itemrating; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- TOC entry 2110 (class 0 OID 0)
+-- Dependencies: 187
+-- Name: itemrating_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('itemrating_id_seq', 1, false);
+
+
+--
+-- TOC entry 2086 (class 0 OID 17319)
+-- Dependencies: 179
 -- Data for Name: menu; Type: TABLE DATA; Schema: public; Owner: -
 --
 
 
 
 --
+-- TOC entry 2111 (class 0 OID 0)
+-- Dependencies: 180
 -- Name: menu_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
@@ -258,18 +412,24 @@ SELECT pg_catalog.setval('menu_id_seq', 1, false);
 
 
 --
+-- TOC entry 2088 (class 0 OID 17324)
+-- Dependencies: 181
 -- Data for Name: menuitem; Type: TABLE DATA; Schema: public; Owner: -
 --
 
 
 
 --
+-- TOC entry 2089 (class 0 OID 17327)
+-- Dependencies: 182
 -- Data for Name: restaurant; Type: TABLE DATA; Schema: public; Owner: -
 --
 
 
 
 --
+-- TOC entry 2112 (class 0 OID 0)
+-- Dependencies: 183
 -- Name: restaurant_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
@@ -277,6 +437,32 @@ SELECT pg_catalog.setval('restaurant_id_seq', 1, false);
 
 
 --
+-- TOC entry 2093 (class 0 OID 17482)
+-- Dependencies: 186
+-- Data for Name: review; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- TOC entry 2113 (class 0 OID 0)
+-- Dependencies: 185
+-- Name: review_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('review_id_seq', 1, false);
+
+
+--
+-- TOC entry 2091 (class 0 OID 17475)
+-- Dependencies: 184
+-- Data for Name: reviewtype; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- TOC entry 1942 (class 2606 OID 17339)
 -- Name: pk_accounttype; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -285,6 +471,7 @@ ALTER TABLE ONLY accounttype
 
 
 --
+-- TOC entry 1944 (class 2606 OID 17341)
 -- Name: pk_item; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -293,6 +480,7 @@ ALTER TABLE ONLY item
 
 
 --
+-- TOC entry 1946 (class 2606 OID 17343)
 -- Name: pk_itemcategory; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -301,6 +489,16 @@ ALTER TABLE ONLY itemcategory
 
 
 --
+-- TOC entry 1959 (class 2606 OID 17509)
+-- Name: pk_itemrating; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY itemrating
+    ADD CONSTRAINT pk_itemrating PRIMARY KEY (id);
+
+
+--
+-- TOC entry 1948 (class 2606 OID 17345)
 -- Name: pk_menu; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -309,6 +507,7 @@ ALTER TABLE ONLY menu
 
 
 --
+-- TOC entry 1950 (class 2606 OID 17347)
 -- Name: pk_menuitem; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -317,6 +516,7 @@ ALTER TABLE ONLY menuitem
 
 
 --
+-- TOC entry 1952 (class 2606 OID 17349)
 -- Name: pk_restaurant; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -325,6 +525,25 @@ ALTER TABLE ONLY restaurant
 
 
 --
+-- TOC entry 1957 (class 2606 OID 17490)
+-- Name: pk_review; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY review
+    ADD CONSTRAINT pk_review PRIMARY KEY (id);
+
+
+--
+-- TOC entry 1954 (class 2606 OID 17479)
+-- Name: pk_reviewtype; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY reviewtype
+    ADD CONSTRAINT pk_reviewtype PRIMARY KEY (id);
+
+
+--
+-- TOC entry 1940 (class 2606 OID 17351)
 -- Name: pk_user; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -333,6 +552,15 @@ ALTER TABLE ONLY account
 
 
 --
+-- TOC entry 1955 (class 1259 OID 17501)
+-- Name: fki_review_user; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX fki_review_user ON review USING btree (account);
+
+
+--
+-- TOC entry 1961 (class 2606 OID 17352)
 -- Name: fk_item_restaurant; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -341,6 +569,25 @@ ALTER TABLE ONLY item
 
 
 --
+-- TOC entry 1969 (class 2606 OID 17510)
+-- Name: fk_itemrating_item; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY itemrating
+    ADD CONSTRAINT fk_itemrating_item FOREIGN KEY (item) REFERENCES item(id);
+
+
+--
+-- TOC entry 1970 (class 2606 OID 17515)
+-- Name: fk_itemrating_user; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY itemrating
+    ADD CONSTRAINT fk_itemrating_user FOREIGN KEY (account) REFERENCES account(id);
+
+
+--
+-- TOC entry 1962 (class 2606 OID 17357)
 -- Name: fk_menu_restaurant; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -349,6 +596,7 @@ ALTER TABLE ONLY menu
 
 
 --
+-- TOC entry 1963 (class 2606 OID 17362)
 -- Name: fk_menuitem_item; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -357,6 +605,7 @@ ALTER TABLE ONLY menuitem
 
 
 --
+-- TOC entry 1964 (class 2606 OID 17367)
 -- Name: fk_menuitem_itemcategory; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -365,6 +614,7 @@ ALTER TABLE ONLY menuitem
 
 
 --
+-- TOC entry 1965 (class 2606 OID 17372)
 -- Name: fk_menuitem_menu; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -373,6 +623,7 @@ ALTER TABLE ONLY menuitem
 
 
 --
+-- TOC entry 1966 (class 2606 OID 17377)
 -- Name: fk_restaurant_user; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -381,12 +632,33 @@ ALTER TABLE ONLY restaurant
 
 
 --
+-- TOC entry 1967 (class 2606 OID 17491)
+-- Name: fk_review; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY review
+    ADD CONSTRAINT fk_review FOREIGN KEY (parent_type) REFERENCES reviewtype(id);
+
+
+--
+-- TOC entry 1968 (class 2606 OID 17496)
+-- Name: fk_review_user; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY review
+    ADD CONSTRAINT fk_review_user FOREIGN KEY (account) REFERENCES account(id);
+
+
+--
+-- TOC entry 1960 (class 2606 OID 17382)
 -- Name: fk_user_type; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY account
     ADD CONSTRAINT fk_user_type FOREIGN KEY (type) REFERENCES accounttype(id);
 
+
+-- Completed on 2016-12-11 15:11:30 CET
 
 --
 -- PostgreSQL database dump complete
